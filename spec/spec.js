daff94cd691c5262ce26121387feed11ab15dff3
Dem Pilafian <dem@centerkey.com>
2020-01-08 01:45:39 -0800

    Fix alignment

--- spec/spec.js
+++ spec/spec.js
@@ -19,28 +19,32 @@ const html = `
 </html>
 `;
 
-const { JSDOM } =    require('jsdom');
-const window =       new JSDOM(html).window;
-const $ =            require('jquery')(window);
-const dna =          require('dna.js')(window, $);
-const app =          require('../app.js')(window, $, dna);
+const { JSDOM } = require('jsdom');
+const window =    new JSDOM(html).window;
+const $ =         require('jquery')(window);
+const dna =       require('dna.js')(window, $);
+const app =       require('../app.js')(window, $, dna);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 describe('Utility function dna.array.fromMap()', () => {
+
    it('converts a map into an array of maps', () => {
       const map = { a: { word: 'Ant' }, b: { word: 'Bat' } };
       const actual =   dna.array.fromMap(map, { key: 'letter' });
       const expected = [{ word: 'Ant', letter: 'a' }, { word: 'Bat', letter: 'b' }];
       expect(actual).toEqual(expected);
       });
+
    });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 describe('Lunch', () => {
+
    it('is ording and eating bulgogi', () => {
       app.doLunch();
       const actual =   dna.getModel('task');
       const expected = [{ title: 'Order bulgogi' }, { title: 'Eat bulgogi' }];
       expect(actual).toEqual(expected);
       });
+
    });
