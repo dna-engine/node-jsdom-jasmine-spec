9c4f422c6d389371d5af9ec6ee15c3e51c8f513a
dpilafian <dem@centerkey.com>
2021-06-16 13:12:29 -0700

    Add workflows build action

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ _Sample dna.js project running on Node.js with jsdom executing Jasmine test case
 
 [![License:MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://dnajs.org/license)
 [![Vulnerabilities](https://snyk.io/test/github/dnajs/dnajs-node-jsdom-jasmine-spec/badge.svg)](https://snyk.io/test/github/dnajs/dnajs-node-jsdom-jasmine-spec)
-[![Build](https://travis-ci.org/dnajs/dnajs-node-jsdom-jasmine-spec.svg)](https://travis-ci.org/dnajs/dnajs-node-jsdom-jasmine-spec)
+[![Build](https://github.com/dnajs/dnajs-node-jsdom-jasmine-spec/workflows/build/badge.svg)](https://github.com/dnajs/dnajs-node-jsdom-jasmine-spec/actions?query=workflow%3Abuild)
 
 [dnajs.org](https://dnajs.org)
 
---
+++ spec/spec.esm.js
@@ -0,0 +1,55 @@
+///////////////////////////////////
+// dnajs-node-jsdom-jasmine-spec //
+///////////////////////////////////
+
+// Run:
+//    $ cd dnajs-node-jsdom-jasmine-spec
+//    $ npm test
+
+// Imports
+import { JSDOM } from 'jsdom';
+import jQuery from 'jquery';
+import { dna } from 'dnajs';
+import { app } from '../app.js';
+
+// Setup
+const html = `
+   <!doctype html>
+   <html lang=en>
+      <head>
+         <meta charset=utf-8>
+         <title>Specification Runner</title>
+      </head>
+      <body>
+         <h2 id=task class=dna-template>~~title~~</h2>
+      </body>
+   </html>
+   `;
+const dom = new JSDOM(html);
+const $ =   jQuery(dom.window);
+dna.initGlobal(dom.window, $);
+app.init(dom.window, $, dna);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('Utility function dna.array.fromMap()', () => {
+
+   it('converts a map into an array of maps', () => {
+      const map = { a: { word: 'Ant' }, b: { word: 'Bat' } };
+      const actual =   dna.array.fromMap(map, { key: 'letter' });
+      const expected = [{ word: 'Ant', letter: 'a' }, { word: 'Bat', letter: 'b' }];
+      expect(actual).toEqual(expected);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('Lunch', () => {
+
+   it('is ording and eating bulgogi', () => {
+      app.doLunch();
+      const actual =   dna.getModel('task');
+      const expected = [{ title: 'Order bulgogi' }, { title: 'Eat bulgogi' }];
+      expect(actual).toEqual(expected);
+      });
+
+   });
